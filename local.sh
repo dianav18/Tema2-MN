5b4d933b8d6613e64981623f22ddd8dccde36c14
Andrei Stan <standrei2003@yahoo.com>
2024-05-08 01:06:45 +0300

    Changed project group.
    Signed-off-by: Andrei Stan <standrei2003@yahoo.com>

--- local.sh
+++ local.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")" || exit 1
 RED='\033[0;31m'
 NC='\033[0m'
 
-DEFAULT_IMAGE_NAME="mn/2023-2024/tema2-private"
+DEFAULT_IMAGE_NAME="mn/2023-2024/tema2/tema2-private"
 DEFAULT_TAG='latest'
 DEFAULT_REGISTRY='gitlab.cs.pub.ro:5050'
 
